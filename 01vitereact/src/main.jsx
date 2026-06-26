086a9e4ef6920a2e9320959ea73ee96862fe44a4
Atulkumar58 <atulchaudhary151@gmail.com>
2026-06-26 15:34:22 +0530

    feat: refactor main.jsx to include MyApp component and additional elements

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+function MyApp(){
+  return (
+    <>
+     <h1>Welcome to Vite from React</h1>
+    </>
+  )
+}
+
+const anotherElement = (
+  <a href="https://www.google.com" target="_blank"> Visit Google from the object </a>
+)
+
+ const reactElement = React.createElement(
+    'a',
+     {
+        href: 'https://www.google.com',
+        target: '_blank'
+    },
+      'Click here to go to Google'
+    )
+  //attributes of createElement are tagname, object of attributes, children
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <MyApp />
     <App />
-  </StrictMode>,
+    {anotherElement}
+    <br />
+    {reactElement}
+  </>
+  
 )
