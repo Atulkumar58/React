889fb9806c829f2acb541e843b90151a57b66031
Atulkumar58 <atulchaudhary151@gmail.com>
2026-06-26 14:44:44 +0530

    feat: initialize Vite React project with essential files and configurations
    - Added package.json with dependencies and scripts for development, build, and linting.
    - Created favicon.svg and icons.svg for application branding.
    - Implemented main application structure in App.jsx and added a simple component Chai.jsx.
    - Configured Vite with a basic setup in vite.config.js.
    - Established entry point in main.jsx for rendering the application.
    - Generated package-lock.json for dependency management.

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+import Chai from './chai'
+function App() {
+  
+
+  return (
+    <>
+     <h1>Welcome to Vite from React | Atul Chaudhary</h1>
+    <Chai/>
+    </>
+    // <h1>Welcome to Vite from React | Atul Chaudhary</h1>
+    // <><Chai/></>
+    // <Chai/>
+  )
+}
+
+export default App
