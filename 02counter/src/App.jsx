fb238572da897019095d88d9cb3036e0ac5ba2b4
Atulkumar58 <atulchaudhary151@gmail.com>
2026-06-28 11:01:02 +0530

    feat: update counter functionality and enhance button styles

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,29 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
+
 import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  let count =5;
+
+  const [count, setCounter]=useState(15)
+
+  // let count =5;
+
   function addvalue() {
-    count++;
-    console.log(count+ "add value");
+    setCounter(count + 1);
+    console.log(count);
   }
   function removevalue() {
-    count--;
-    console.log(count+ "remove value");
+    setCounter(count - 1);
+    console.log(count);
   }
   return (
     <>
       <h1>Counter App</h1>
       <h2>Counter Value: {count}</h2>
-
+      
       <button
-      onClick={addvalue}
-      >Add Value</button> <br/>
+      onClick={addvalue }
+      >Add Value {count}</button> <br/>
       <button 
-      onClick={removevalue}>remove Value</button>
+      onClick={removevalue}>remove Value {count}</button>
     </>
   )
 }
