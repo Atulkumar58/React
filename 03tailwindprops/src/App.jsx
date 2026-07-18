43daaa6f72be10c7bfe15ad10eea32236f9fd471
Atulkumar58 <atulchaudhary151@gmail.com>
2026-07-18 11:44:06 +0530

    feat: initialize Tailwind CSS project with React and Vite
    - Added main application structure with App.jsx and main.jsx
    - Integrated Tailwind CSS for styling with a custom configuration
    - Created reusable Card component for displaying user information
    - Implemented global styles and CSS variables for theming
    - Added SVG assets for React and Vite logos
    - Developed CSS styles for layout and components
    - Set up responsive design for various screen sizes

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import Card from './components/card'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const obj = {
+    name: 'Atul',
+    age: 22,
+    city: 'Bangalore'
+  }
+  let array = [1, 2, 3, 4, 5]
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
+      import React from 'react'
+  
+      <Card userName='chai or code' /><br /><br/>
+      <Card userName='Atul' />
+    </>
+  )
+}
+
+export default App
